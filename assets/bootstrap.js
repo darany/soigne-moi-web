c31b9ac36f81c6ebda0c502dae260bb645934ac5
Benjamin BALET <benjamin.balet@gmail.com>
2023-10-22 22:32:04 +0200

    first commit

---
+++ assets/bootstrap.js
@@ -0,0 +1,17 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.[jt]sx?$/
+));
+
+// this waits for Turbo Drive to load
+document.addEventListener('turbo:load', function (e) {
+    // this enables bootstrap tooltips globally
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    });
+});
